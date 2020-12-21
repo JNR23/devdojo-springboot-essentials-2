b1e8eb9aeec6a6e58051661544c9a17400e5e2c2
JNR23 <junior_bhzte@hotmail.com>
2020-12-21 16:15:47 -0300

    Validation Fields

--- src/main/java/com/jnr23/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/com/jnr23/springboot/requests/AnimePostRequestBody.java
@@ -2,9 +2,13 @@ package com.jnr23.springboot.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
 
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 
 }
