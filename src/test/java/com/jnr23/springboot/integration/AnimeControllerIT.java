25727f037b07bab5a4b6b14f023914aae3b79ffa
JNR23 <junior_bhzte@hotmail.com>
2020-12-27 15:25:51 -0300

    Test Integration

---
+++ src/test/java/com/jnr23/springboot/integration/AnimeControllerIT.java
@@ -0,0 +1,44 @@
+package com.jnr23.springboot.integration;
+
+import com.jnr23.springboot.domain.Anime;
+import com.jnr23.springboot.repository.AnimeRepository;
+import com.jnr23.springboot.util.AnimeCreator;
+import com.jnr23.springboot.wrapper.PageableResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+public class AnimeControllerIT {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("List returns list of anime inside page object when successful")
+    void list_ReturnsListOfAnimesInsidePageObject_WhenSuccessful() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createAnimeToBeSaved());
+        String expectedName = savedAnime.getName();
+        PageableResponse<Anime> animePage = testRestTemplate.exchange("/animes", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Anime>>() {
+                }).getBody();
+        Assertions.assertThat(animePage).isNotNull();
+        Assertions.assertThat(animePage.toList()).isNotEmpty().hasSize(1);
+        Assertions.assertThat(animePage.toList().get(0).getName()).isEqualTo(expectedName);
+    }
+
+}
