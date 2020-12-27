a6394cefd1a949bacec87e44c0b4eb3823922762
JNR23 <junior_bhzte@hotmail.com>
2020-12-27 20:34:50 -0300

    Maven Profile for Tests Integration

--- src/test/java/com/jnr23/springboot/SpringbootEssentialsApplicationTests.java
+++ src/test/java/com/jnr23/springboot/SpringbootEssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package com.jnr23.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootEssentialsApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
